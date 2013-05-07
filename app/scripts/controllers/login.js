d4adcaa7fc5fa09d0750e49a7155222461fb6822
Jaime Febres <jaime.febres@gmail.com>
2013-05-07 17:07:24 -0500

    Set page title and module info in the login controller

--- app/scripts/controllers/login.js
+++ app/scripts/controllers/login.js
@@ -1,7 +1,11 @@
 'use strict';
 
 angular.module('spaYougradeApp')
-  .controller('LoginCtrl', function ($scope) {
+  .controller('LoginCtrl', function ($scope,PageTitle,ModuleInfoService) {
+  	PageTitle.setTitle('Login');
+    ModuleInfoService.moduleTitle = 'Login';
+    ModuleInfoService.moduleDescription = 'Enter your credentials below to access the system';
+
     $scope.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
