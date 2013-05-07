59f420259be0da7dcce87ff59f244ff27eed7285
Jaime Febres <jaime.febres@gmail.com>
2013-05-06 19:06:58 -0500

    yo angular

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('spaYougradeApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
