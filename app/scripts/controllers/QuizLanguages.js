81790cec8cbcda671b830dd5d4f00d7f58908cf7
Jaime Febres <jaime.febres@gmail.com>
2013-05-08 16:45:27 -0500

    Removed unused files

--- app/scripts/controllers/QuizLanguages.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-
-angular.module('spaYougradeApp')
-  .controller('QuizLanguagesCtrl', function ($scope,QuizLanguageService) {
-    $scope.panes = QuizLanguageService.availableLanguages();
-  });
