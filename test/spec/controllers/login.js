926ade77d31e5267d7e6084c7e0a6cebd233f015
Jaime Febres <jaime.febres@gmail.com>
2013-05-09 14:58:19 -0500

    Test LoginCtrl

--- test/spec/controllers/login.js
+++ test/spec/controllers/login.js
@@ -6,13 +6,31 @@ describe('Controller: LoginCtrl', function () {
   beforeEach(module('spaYougradeApp'));
 
   var LoginCtrl,
-    scope;
+    scope,
+    ModuleInfoService,
+    thePageTitle;
 
   // Initialize the controller and a mock scope
   beforeEach(inject(function ($controller, $rootScope) {
     scope = $rootScope.$new();
+    ModuleInfoService = {};
     LoginCtrl = $controller('LoginCtrl', {
-      $scope: scope
+      $scope: scope,
+      PageTitle : {
+        setTitle: function(title){
+          thePageTitle=title;
+        }
+      },
+      ModuleInfoService : ModuleInfoService
     });
   }));
+  it('sets the page title',function(){
+    expect(thePageTitle).toBe('Login');
+  });
+  it('sets the module title',function(){
+    expect(ModuleInfoService.moduleTitle).toBe('Login');
+  });
+  it('sets the module description',function(){
+    expect(ModuleInfoService.moduleDescription).toBe('Enter your credentials below to access the system');
+  });
 });
