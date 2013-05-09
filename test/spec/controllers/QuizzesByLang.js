beedc9cc1e5a63e44482ab30db0591e744e7e71e
Jaime Febres <jaime.febres@gmail.com>
2013-05-09 14:47:47 -0500

    Test QuizzesByLangCtrl

--- test/spec/controllers/QuizzesByLang.js
+++ test/spec/controllers/QuizzesByLang.js
@@ -6,13 +6,38 @@ describe('Controller: QuizzesByLangCtrl', function () {
   beforeEach(module('spaYougradeApp'));
 
   var QuizzesByLangCtrl,
-    scope;
+    scope,
+    QuizService,
+    theQuizzes,
+    theLang;
 
   // Initialize the controller and a mock scope
   beforeEach(inject(function ($controller, $rootScope) {
     scope = $rootScope.$new();
+    theLang = 'en';
+    QuizService = {
+      getByLang: function(lang){
+        if(lang===theLang){
+          return theQuizzes;
+        }
+        return null;
+      }
+    };
+    theQuizzes = [
+      {name:'q1'},
+      {name:'q2'}
+    ]
+
     QuizzesByLangCtrl = $controller('QuizzesByLangCtrl', {
-      $scope: scope
+      $scope: scope,
+      QuizService: QuizService
     });
   }));
+  it('sets the list of quizzes as empty before calling to init',function(){
+    expect(scope.quizzes).toEqual([]);
+  });
+  it('sets the list of quizzes by lang after calling to init',function(){
+    scope.init(theLang);
+    expect(scope.quizzes).toBe(theQuizzes);
+  });
 });
