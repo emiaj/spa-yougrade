bd8b7ddeac8860a90367359e08babd899a338ff0
Jaime Febres <jaime.febres@gmail.com>
2013-05-09 20:48:10 -0500

    Test ExamService

--- test/spec/services/ExamService.js
+++ test/spec/services/ExamService.js
@@ -10,9 +10,21 @@ describe('Service: ExamService', function () {
   beforeEach(inject(function (_ExamService_) {
     ExamService = _ExamService_;
   }));
-
-  it('should do something', function () {
-    expect(!!ExamService).toBe(true);
+  it('returns the same data for a given exam key', function () {
+    expect(ExamService.dataFor('foo')).not.toBeUndefined();
+    expect(ExamService.dataFor('foo')).toBe(ExamService.dataFor('foo'));
   });
-
+  it('exam data key',function(){
+    expect(ExamService.dataFor('foo').key).toBe('foo');
+  });
+  it('returns the same answer object for a given question',function(){
+    expect(ExamService.dataFor('foo').answerFor(3))
+    .toBe(ExamService.dataFor('foo').answerFor(3));
+  })
+  it('question number',function(){
+    expect(ExamService.dataFor('foo').answerFor(3).question).toBe(3);
+  })
+  it('alternative number by default is -1',function(){
+    expect(ExamService.dataFor('foo').answerFor(3).alternative).toBe(-1);
+  })
 });
