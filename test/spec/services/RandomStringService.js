32f6b37daab5952fd8f2974a3d578c9cc9bbb641
Jaime Febres <jaime.febres@gmail.com>
2013-05-09 20:51:25 -0500

    Test RandomStringService

--- test/spec/services/RandomStringService.js
+++ test/spec/services/RandomStringService.js
@@ -11,8 +11,13 @@ describe('Service: RandomStringService', function () {
     RandomStringService = _RandomStringService_;
   }));
 
-  it('should do something', function () {
-    expect(!!RandomStringService).toBe(true);
+  it('returns a valid string', function () {
+    expect(RandomStringService.getValue(5)).not.toBeUndefined();
   });
-
+  it('returns a string with the specified length', function () {
+    expect(RandomStringService.getValue(5).length).toBe(5);
+  });
+  it('returns a unique string on each call', function () {
+    expect(RandomStringService.getValue(5)).not.toBe(RandomStringService.getValue(5));
+  });
 });
