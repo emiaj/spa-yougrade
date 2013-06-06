b471877a6975c948f696c4e50e7235ca3c6c23ab
Jaime Febres <jaime.febres@gmail.com>
2013-06-05 20:37:06 -0500

    Linked to grading service endpoint

--- app/scripts/services/ExamGradingService.js
+++ app/scripts/services/ExamGradingService.js
@@ -1,38 +1,22 @@
 'use strict';
 
 angular.module('spaYougradeApp')
-  .factory('ExamGradingService', function (ExamService,QuizService,$q) {
-    // Service logic
-    // ...
-    // Public API here
-    return {
-      evaluate: function (quizId,examId) {
-        var deferred = $q.defer();
-        var exam = ExamService.dataFor(examId);
-        var quizAnswers = QuizService.answersFor(quizId);
-        var grade = {
-          questions : quizAnswers.length,
-          correct : 0,
-          incorrect: 0,
-          approved : function(){
-            return this.correct>this.incorrect;
-          }
+    .factory('ExamGradingService', function (ExamService, QuizService, $q,$http) {
+        // Service logic
+        // ...
+        // Public API here
+        return {
+            evaluate: function (quizId, examId) {
+                var deferred = $q.defer();
+                $http
+                    .post('http://localhost:8080/exams/eval', {
+                        quiz: parseInt(quizId,10),
+                        exam: examId
+                    })
+                    .success(function(data){
+                        deferred.resolve(data);
+                    });
+                return deferred.promise;
+            }
         };
-        exam.then(function(data){
-          for (var i = 0; i < quizAnswers.length; i++) {
-            var answer = quizAnswers[i];
-            var userAnswer = data.answerFor(answer.question);
-            if(answer.alternative === parseInt(userAnswer.alternative,10)){
-              grade.correct+=1;
-            }
-            else{
-              grade.incorrect+=1;
-            }
-          }
-          deferred.resolve(grade);
-        });
-
-        return deferred.promise;
-      }
-    };
-  });
+    });
