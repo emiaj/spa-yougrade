b8d03bfefe2c15104c7161bece21525b84cf5f82
Jaime Febres <jaime.febres@gmail.com>
2013-06-05 20:53:42 -0500

    Fixed issues caused by automatic code formatting

--- app/scripts/services/QuizService.js
+++ app/scripts/services/QuizService.js
@@ -1,24 +1,24 @@
 'use strict';
 
-angular.module('spayougradeapp')
-  .factory('quizservice', function ($q,$http) {
-    // service logic
+angular.module('spaYougradeApp')
+  .factory('QuizService', function ($q,$http) {
+    // Service logic
     // ...
-    // public api here
+    // Public API here
     return {
-      getbylang: function (lang) {
+      getByLang: function (lang) {
         var deferred = $q.defer();
         $http
-        .jsonp('http://localhost:8080/quizzes/' + lang + '?callback=json_callback')
+        .jsonp('http://localhost:8080/quizzes/' + lang + '?callback=JSON_CALLBACK')
         .success(function(data){
           deferred.resolve(data);
         });
         return deferred.promise;
       },
-      getbyid: function(id){
+      getById: function(id){
         var deferred = $q.defer();
         $http
-        .jsonp('http://localhost:8080/quizzes/' + id + '?callback=json_callback')
+        .jsonp('http://localhost:8080/quizzes/' + id + '?callback=JSON_CALLBACK')
         .success(function(data){
           deferred.resolve(data);
         });
